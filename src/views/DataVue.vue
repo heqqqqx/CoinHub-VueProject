c513de03833e340361ad3832557c02b0b5e9652d
Amaury DAVOINE <92042040+heqqqqx@users.noreply.github.com>
2023-10-22 18:53:49 +0200

    bug fixes + added change currency button
    Co-Authored-By: Mayeul78 <66210853+mayeul78@users.noreply.github.com>

--- src/views/DataVue.vue
+++ src/views/DataVue.vue
@@ -1,20 +1,19 @@
 <template>
   <div v-if="user">
-    <trading-view :cryptoname="this.$route.params.cryptoname" />
+    <trading-view :key="this.$route.fullPath" :cryptoname="this.$route.params.cryptoname" /> <!--pour may, là on rajoute une clé unique sinon le refresh a pas lieu-->
     <div class="center-container">
       <div class="redirect-button-position">
         <RedirectButton :cryptoname="this.$route.params.cryptoname" />
       </div>
       <Analysis :symbol="`BINANCE:${$route.params.cryptoname}`" />
     </div>
-  
   </div>
   <div v-else class="not-logged-in-message">
     <h1>You are not logged in</h1>
     <p>Please log in to view this page and access the full website.</p>
   </div>
-  
 </template>
+
 
 <script>
 import TradingView from '../components/TradingVue.vue';
@@ -27,6 +26,7 @@ export default {
     const { user } = useAuth0();
     return { user };
   },
+  
   components: { TradingView, Analysis, RedirectButton }
 }
 </script>
